9679407954d050e7ba21d4d45b225d3f8bf5900a
Nick <micron30pushtaev@gmail.com>
2024-05-14 23:36:03 +0400

    Second commit - Fix header - custom button

--- client/src/components/custom-button/index.tsx
+++ client/src/components/custom-button/index.tsx
@@ -1,38 +1,40 @@
-import { Button, Form } from 'antd'
+import { Button, Form } from "antd";
 
 type Props = {
-    children: React.ReactNode;
-    htmlType?: "button" | "submit" | "reset" | undefined;
-    onClick?: () => void;
-    type?: "link" | "text" | "default" | "primary" | "dashed" | undefined;
-    danger?: boolean;
-    loading?: boolean;
-    shape?: "circle" | "default" | "round" | undefined;
-    icon?: React.ReactNode;
-}
+	children: React.ReactNode;
+	htmlType?: "button" | "submit" | "reset" | undefined;
+	onClick?: () => void;
+	type?: "link" | "text" | "default" | "primary" | "dashed" | undefined;
+	danger?: boolean;
+	loading?: boolean;
+	shape?: "circle" | "default" | "round" | undefined;
+	icon?: React.ReactNode;
+};
 
-export const CustomButton = ({ 
-    children, 
-    htmlType = 'button',
-    type,
-    danger,
-    loading,
-    shape,
-    icon,
-    onClick
-}: Props ) => {
-  return (
-    <Form.Item>
-        <Button 
-        htmlType={ htmlType }
-        type={ type }
-        danger={ danger }
-        loading={ loading }
-        size="large"
-        shape={ shape }
-        onClick={ onClick }
-        icon= { icon }
-        >{ children }</Button>
-    </Form.Item>
-  )
-}
+export const CustomButton = ({
+	children,
+	htmlType = "button",
+	type,
+	danger,
+	loading,
+	shape,
+	icon,
+	onClick,
+}: Props) => {
+	return (
+		<Form.Item>
+			<Button
+				htmlType={htmlType}
+				type={type}
+				danger={danger}
+				loading={loading}
+				size="large"
+				shape={shape}
+				onClick={onClick}
+				icon={icon}
+			>
+				{children}
+			</Button>
+		</Form.Item>
+	);
+};
--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,13 +1,16 @@
 import { Layout, Space, Typography, Button } from 'antd';
 import styles from './index.module.css'
 import { TeamOutlined } from '@ant-design/icons';
+import { CustomButton } from '../custom-button';
 
 export const Header = () => {
   return (
     <Layout.Header className={ styles.teamIcon}>
       <Space>
         <TeamOutlined className={ styles.teamIcon} />
-        <Button type='primary'>Click</Button>
+        <CustomButton type='primary'>
+          <Typography.Title level={1}>Сотрудники</Typography.Title>
+        </CustomButton>
       </Space>
     </Layout.Header>
   )
